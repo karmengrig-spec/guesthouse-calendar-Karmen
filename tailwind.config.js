44fcd34eda336c384c0809eded2506d519459d8a
karmengrig-spec <karmengrig@gmail.com>
2025-10-23 22:21:02 +0400

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,1 @@
-module.exports = { content: ["./src/**/*.{js,jsx,ts,tsx,html}"], theme: { extend: {} }, plugins: [] };
+module.exports={content:["./src/**/*.{js,jsx,ts,tsx,html}"],theme:{extend:{}},plugins:[]};
